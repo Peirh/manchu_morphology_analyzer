601952e65aa62e4d0b489ee26049f6637b51a526
Pei <2haraldhardrada@gmail.com>
2024-08-09 23:09:34 +0200

    modified setup.py, so that now when installing, the data folder will be downloaded

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version='1.0',                 # Required: Package version
     packages=find_packages(),        # Automatically find and include all packages
     python_requires='>=3.8.5',       # Required: Specify the minimum Python version
+    package_data={
+        'manchu_morphology_analyzer': ['data/*.txt'],  # Include all .txt files in the data folder
+    },
+    include_package_data=True,      # Ensure that package_data is included
 )
