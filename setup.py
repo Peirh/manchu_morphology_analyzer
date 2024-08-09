bc18b93a795e1f305ae74f2455d5510ba773cb29
Pei <2haraldhardrada@gmail.com>
2024-08-09 23:43:02 +0200

    update version number to 1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manchu_morphology_analyzer',              # Required: Name of the package
-    version='1.0',                 # Required: Package version
+    version='1.1',                 # Required: Package version
     packages=find_packages(),        # Automatically find and include all packages
     python_requires='>=3.8.5',       # Required: Specify the minimum Python version
     package_data={
